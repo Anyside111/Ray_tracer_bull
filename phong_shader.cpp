258a1e35f9a3746cf7795004f856227e6f008e56
Anyside111 <YiifangRen@gmail.com>
2023-03-15 01:26:33 -0700

    3.15

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -5,10 +5,26 @@
 #include "render_world.h"
 
 vec3 Phong_Shader::
-Shade_Surface(const Ray& ray,const vec3& intersection_point,
-    const vec3& normal,int recursion_depth) const
-{
+Shade_Surface(const Ray &ray, const vec3 &intersection_point,
+              const vec3 &normal, int recursion_depth) const {
     vec3 color;
-    TODO; //determine the color
+    vec3 c_ambient = this->world.ambient_color * this->world.ambient_intensity * color_ambient;
+    vec3 c_diffuse;
+    vec3 c_specular;
+    for (int i = 0; i < this->world.lights.size(); i++) {
+        auto incident = this->world.lights[i]->brightness * this->world.lights[i]->color;
+        vec3 L =  (ray.endpoint - intersection_point).normalized();
+        c_diffuse += incident * fmax(0, dot(normal, L)) * color_diffuse/ (ray.endpoint - intersection_point).magnitude_squared();
+
+        double p = this->specular_power;
+        vec3 V = (intersection_point - ray.endpoint).normalized();
+        vec3 R = 2.0 * dot(V, normal) * normal - V;
+        c_specular += incident * fmax(0, pow(dot(R, V), p));
+
+    }
+
+
+    //color = c_ambient + c_diffuse + c_specular;
+    color =  c_diffuse;
     return color;
 }
--- plane.cpp
+++ plane.cpp
@@ -7,25 +7,26 @@
 // Intersect with the half space defined by the plane.  The plane's normal
 // points outside.  If the ray starts on the "inside" side of the plane, be sure
 // to record a hit with t=0 as the first entry in hits.
-Hit Plane::Intersection(const Ray& ray, int part) const
-{
+Hit Plane::Intersection(const Ray &ray, int part) const {
+    double dom = dot(ray.direction, normal);
+    double distance = 10e5;
+    if (dom < 10e-5) {
+        distance = dot((x1 - ray.endpoint), normal) / dom;
+    }
 
-
-    return {0,0,0};
+    return {0, distance, 0};
 }
 
-vec3 Plane::Normal(const vec3& point, int part) const
-{
+vec3 Plane::Normal(const vec3 &point, int part) const {
     return normal;
 }
 
 // There is not a good answer for the bounding box of an infinite object.
 // The safe thing to do is to return a box that contains everything.
-Box Plane::Bounding_Box(int part) const
-{
+Box Plane::Bounding_Box(int part) const {
     Box b;
     b.hi.fill(std::numeric_limits<double>::max());
-    b.lo=-b.hi;
+    b.lo = -b.hi;
     return b;
 }
 
