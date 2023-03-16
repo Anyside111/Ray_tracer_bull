2967046ee9413e4819e750aa9e5bbb5e68d9ddff
Anyside111 <YiifangRen@gmail.com>
2023-03-16 00:36:51 -0700

    3.15-3

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -14,15 +14,15 @@ Shade_Surface(const Ray &ray, const vec3 &intersection_point,
     for (int i = 0; i < this->world.lights.size(); i++) {
         auto incident = this->world.lights[i]->brightness * this->world.lights[i]->color;
         vec3 lightDir =  (this->world.lights[i]->position - intersection_point).normalized();
+        double lightIntensity_factor = 4* M_PI *(this->world.lights[i]->position - intersection_point).magnitude_squared();
         vec3 norm = normal.normalized();
-        c_diffuse += incident * fmax(0, dot(norm, lightDir)) *color_diffuse;
+        c_diffuse += incident * fmax(0, dot(norm, lightDir)) *color_diffuse/lightIntensity_factor;
 
         double p = this->specular_power;
         vec3 viewDir = (this->world.camera.position - intersection_point).normalized();
         vec3 reflectDir = -lightDir - 2.0 * dot(-lightDir,norm)*norm;
-        c_specular += incident * pow(fmax(dot(viewDir,reflectDir), 0) , p) *color_specular;
+        c_specular += incident * pow(fmax(dot(viewDir,reflectDir), 0) , p) *color_specular/lightIntensity_factor;
     }
-    //color = c_ambient + c_diffuse + c_specular;
-    color =  c_diffuse ;
+    color =  c_ambient + c_diffuse + c_specular;
     return color;
 }
